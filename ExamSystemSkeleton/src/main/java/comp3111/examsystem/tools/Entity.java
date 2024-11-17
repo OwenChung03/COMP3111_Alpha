258da0566e04596e3838066dc35ec785e134ed94
Terry <terryaykc@gmail.com>
2024-11-17 11:13:32 +0800

    k1-student_login_v4

---
+++ ExamSystemSkeleton/src/main/java/comp3111/examsystem/tools/Entity.java
@@ -0,0 +1,28 @@
+package comp3111.examsystem.tools;
+
+import java.lang.reflect.Member;
+
+public class Entity implements java.io.Serializable, Comparable<Member> {
+    protected Long id = 0L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Entity() {
+        super();
+    }
+
+    public Entity(Long id) {
+        super();
+        this.id = id;
+    }
+
+    public int compareTo(Member o) {
+        return Long.compare(this.id, o.id);
+    }
+}
